9721de9283bf545f5a4d2a52c61bff62b9275de4
Lewis Wilson <lewiswilson1998@hotmail.co.uk>
2022-02-03 17:27:09 +0000

    Filled out rest of ProductPage content

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import ProductPage from './pages/ProductPage';
 
+// JSONs
 import DinnerwareProducts from './products/Dinnerware';
 import GlasswareProducts from './products/Glassware';
+import ServewareProducts from './products/Serveware';
+import FlatwareProducts from './products/Flatware';
+import TextileProducts from './products/Textiles';
+import BundleProducts from './products/Bundles';
 
 // TODO
 // Fix footer
@@ -33,6 +38,38 @@ function App() {
             bio="Made in Japan, our soda-lime glassware is lightweight, scratch-resistant, 
                 and durable enough for everyday use—and complements any table setting without stealing the show."
             productList= {GlasswareProducts}
+          />
+        }/>
+        <Route path="/serveware" element={
+          <ProductPage 
+            title="Serveware"
+            bio="Durably crafted and made to last, our serveware adds a refined
+                look to weeknight dinners and special occasions alike."
+            productList= {ServewareProducts}
+          />
+        }/>
+        <Route path="/flatware" element={
+          <ProductPage 
+            title="Flatware"
+            bio="Crafted by the Herdmar family in the Porto region of Portugal, 
+                  our flatware is restaurant quality and treated to last for years of dishwasher cycles."
+            productList= {FlatwareProducts}
+          />
+        }/>
+        <Route path="/textiles" element={
+          <ProductPage 
+            title="Textiles"
+            bio="Our textiles are woven in Belgium from 100% pure linen, and make a beautiful 
+                (and long-lasting) addition to any tabletop."
+            productList= {TextileProducts}
+          />
+        }/>
+        <Route path="/bundles" element={
+          <ProductPage 
+            title="Bundles"
+            bio="Looking to make the perfect set, but don’t know where to start? Look no further.
+                We’ve bundled our collections to make it easy to set your table and save."
+            productList= {BundleProducts}
           />
         }/>
       </Routes>
